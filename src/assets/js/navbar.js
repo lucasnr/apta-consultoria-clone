5a82cf60f35401417e84d5dac357476c88eff9c7
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-05-08 17:12:54 -0300

    Fix accessibility issues

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,22 +1,32 @@
 const navbar = $('#navbar');
-const navbarHeight = navbar.outerHeight();
 
-function toggleNavbar() {
-	navbar.toggleClass('show');
-}
+configureNavbar();
 
-const links = $('.navbar__nav li a');
-links.each(function (index, element) {
-	const $element = $(element);
-	$element.click(function (event) {
-		event.preventDefault();
-
-		if (navbar.hasClass('show')) navbar.removeClass('show');
-
-		const $target = $($element.attr('href'));
-		$('html, body').animate(
-			{ scrollTop: $target.offset().top - navbarHeight },
-			500
-		);
+function configureNavbar() {
+	const button = navbar.find('#navbar-toggle-button');
+	button.click(function () {
+		const expanded = $(this).attr('aria-expanded') === 'true';
+		$(this).attr('aria-expanded', String(!expanded));
+		navbar.toggleClass('show');
 	});
-});
+	button.keypress(function (e) {
+		if (e.which === 13) this.click();
+	});
+
+	const navbarHeight = navbar.outerHeight();
+	const links = $('.navbar__nav li a');
+	links.each(function (index, element) {
+		const $element = $(element);
+		$element.click(function (event) {
+			event.preventDefault();
+
+			if (navbar.hasClass('show')) navbar.removeClass('show');
+
+			const $target = $($element.attr('href'));
+			$('html, body').animate(
+				{ scrollTop: $target.offset().top - navbarHeight },
+				500
+			);
+		});
+	});
+}
